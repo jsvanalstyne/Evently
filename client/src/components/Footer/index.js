324c631977939b359c6bb62d74034250d4d91d39
Jerrica VanAlstyne <jsnow07@yahoo.com>
2020-01-31 14:28:41 -0500

    adjusted footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,8 +3,8 @@ import "./style.css";
 
 function Footer() {
   return (
-    <footer className="footer text-center sticky-bottom">
-      <span className="sticky-bottom">Evently &copy; 2020</span>
+    <footer className="footer text-center sticky-bottom pt-3">
+      <span>Evently &copy; 2020</span>
     </footer>
   );
 }
