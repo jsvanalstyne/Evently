79487f5187c29c14c481bc7dc0b67547cb34adb8
Peter Connelly <peter@Peters-MacBook-Pro.local>
2020-02-01 07:26:18 -0500

    asdf

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var express = require("express");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3001;
 
 // Connect to the Mongo DB
-const dbconn = process.env.MONGO_URL || "mongodb://localhost/Evently"
-mongoose.connect(dbconn, { useNewUrlParser: true });
+// const dbconn = process.env.MONGO_URL || "mongodb://localhost/Evently"
+// mongoose.connect(dbconn, { useNewUrlParser: true });
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+
 
 app.listen(PORT, () => {
     console.log("hello, world!");
