7e3493ab467a7f357beac8f626a2f29ca2a34506
Peter Connelly <peter@Peters-MacBook-Pro.local>
2020-02-02 13:55:19 -0500

    adding Events resource

--- utils/API/test.js
+++ utils/API/test.js
@@ -1,11 +1,11 @@
-var API = require("./Conversations.js");
+var API = require("./Events.js");
 var mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost/Balls');
 
 
 const conversations = ["5e35c71607cf87e4497c4189", "5e35c71607cf87e4497c4187", "5e35c71607cf87e4497c4188"];
 
-API.addUser("5e35c71607cf87e4497c418f", "5e36e6455c2d3ff91fe7a39a", result => {
+API.getEventById("5e35c71607cf87e4497c41bf", result => {
     console.log(result);
 })
     // API.findById("5e36e6455c2d3ff91fe7a39a", result => {
