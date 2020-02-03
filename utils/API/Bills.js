b6be2dca0cf310e063c9757773eba1d5c6021d24
Peter Connelly <peter@Peters-MacBook-Pro.local>
2020-02-02 22:16:12 -0500

    adding organizations and programs resource

--- utils/API/Bills.js
+++
@@ -1,5 +0,0 @@
-const Bills = require("../../models/Bills.js");
-
-module.exports = {
-    
-}
--- utils/API/test.js
+++ utils/API/test.js
@@ -1,11 +1,13 @@
-var API = require("./Events.js");
+var API = require("./Facilities.js");
 var mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost/Balls');
 
 
-const conversations = ["5e35c71607cf87e4497c4189", "5e35c71607cf87e4497c4187", "5e35c71607cf87e4497c4188"];
+const conversations = ["5e35c71607cf87e4497c41a9", "5e35c71607cf87e4497c4187", "5e35c71607cf87e4497c4188"];
+let startDate = Date.now() - 10;
+let endDate = Date.now() + 15;
 
-API.getEventById("5e35c71607cf87e4497c41bf", result => {
+API.getAvailableFacilities("5e35c71607cf87e4497c41a9", startDate, endDate, result => {
     console.log(result);
 })
     // API.findById("5e36e6455c2d3ff91fe7a39a", result => {
