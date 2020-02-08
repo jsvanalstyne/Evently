1efffd9c9123592a8b6bea17dcdd53f4be9aafa2
Peter Connelly <peter@Peters-MacBook-Pro.local>
2020-02-07 20:04:49 -0500

    adsfasdf

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,15 +9,23 @@ router.post("/", (req, res) => {
     let user = req.body.user;
     // account object to send to 
     let account = req.body.account;
-
+    console.log("got in the post route")
+    console.log(user);
+    console.log(account)
     oktaClient.createUser(user)
         .then(newUser => {
+            console.log("new user created");
+            console.log(user);
             accountController.create(account, result => {
+                console.log("account created");
+                console.log(result);
                 let user = {
                     "authId": newUser.id
                 }
 
                 userController.create(user, result => {
+                    console.log("user created");
+                    console.log(result);
                     return res.json({
                         "message": "This shit wurk"
                     }).status(200)
