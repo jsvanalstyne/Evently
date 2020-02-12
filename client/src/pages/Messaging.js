347d81911c4774dafb2be4ccfb37fdb3699ebf70
Peter Connelly <peter@Peters-MacBook-Pro.local>
2020-02-12 18:29:07 -0500

    creating messaging component

---
+++ client/src/pages/Messaging.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Messaging extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <div>Hello!</div>
+        )
+    }
+}
+
+export default Messaging;
