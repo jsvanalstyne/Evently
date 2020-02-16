bba34485f4283903c683f4b3e3d9b75f98f50a9f
Peter Connelly <peter@Peters-MacBook-Pro.local>
2020-02-16 01:37:58 -0500

    users can now make their own conversations

---
+++ client/src/components/Messaging/Recipient/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+
+function Recipient(props) {
+
+    console.log(props);
+    return (
+        <div>
+            <p style={{display: "inline"}}>{props.name}</p>
+            <button style={{display: "inline"}} onClick={event => props.deleteRecipient(event, props.id)}>x</button>
+        </div>
+    )
+}
+
+export default Recipient;
