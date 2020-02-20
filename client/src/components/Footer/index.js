1f62cc004327ecac602bf6d1b9526f56385eedf4
Peter Connelly <peter@Peters-MacBook-Pro.local>
2020-02-20 00:18:48 -0500

    asdf

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function Footer() {
   return (
-    <footer className="footer text-center fixed-bottom pt-3">
+    <footer className="footer text-center fixed-bottom pt-3 custom-footer-color">
       <span>Evently &copy; 2020</span>
     </footer>
   );
